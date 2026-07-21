95cdc9fca8dc54a652ce6023bc07d4c7be618f3d
sakedontrail <hanoo980616@gmail.com>
2026-07-21 14:26:24 +0900

    260721-2

--- js/11wiki.js
+++ js/11wiki.js
@@ -2,6 +2,7 @@
 let a = document.querySelector(".categoryBr1");
 let b = document.querySelector(".categoryBr2");
 let c = document.querySelector(".categoryBr3");
+let d = document.querySelector(".categoryBr4");
 
 
 window.onresize = function(event){
@@ -9,4 +10,5 @@ window.onresize = function(event){
     innerWidth <= "992" ? a.innerHTML = "<br>" : a.innerHTML = "&nbsp;";
     innerWidth <= "992" ? b.innerHTML = "<br>" : b.innerHTML = "&nbsp;";
     innerWidth <= "992" ? c.innerHTML = "<br>" : c.innerHTML = "&nbsp;";
+    innerWidth <= "992" ? d.innerHTML = "<br>" : d.innerHTML = "&nbsp;";
 }
