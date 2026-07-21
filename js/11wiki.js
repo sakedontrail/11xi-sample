cb5fb244fab74cc15d42679f5043dbb10804eeee
sakedontrail <hanoo980616@gmail.com>
2026-07-21 19:06:44 +0900

    260721-3

--- js/11wiki.js
+++ js/11wiki.js
@@ -1,4 +1,5 @@
 // 화면비 따른 줄바꿈
+
 let a = document.querySelector(".categoryBr1");
 let b = document.querySelector(".categoryBr2");
 let c = document.querySelector(".categoryBr3");
@@ -12,3 +13,26 @@ window.onresize = function(event){
     innerWidth <= "992" ? c.innerHTML = "<br>" : c.innerHTML = "&nbsp;";
     innerWidth <= "992" ? d.innerHTML = "<br>" : d.innerHTML = "&nbsp;";
 }
+
+// 자주 쓰는 용어들 검색
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const searchInput = document.getElementById("searchInput");
+    const ffxiWord = document.querySelectorAll(".ffxi-word");
+
+    searchInput.addEventListener("input", function() {
+
+        const searchTerm = this.value.toLowerCase();
+        ffxiWord.forEach((item) =>{
+            const title = item.querySelector("span").textContent.toLowerCase();
+            if(title.includes(searchTerm)) {
+                item.style.display = "block" ;
+            } else {
+                item.style.display = "none";
+            }
+        })
+
+
+    })
+})
